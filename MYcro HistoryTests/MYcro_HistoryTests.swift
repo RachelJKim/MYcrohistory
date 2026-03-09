10cc6d8d8f1e096226aa721be05002ca0bcced99
Rachel J.Kim <racheljk02@gmail.com>
2026-03-09 20:48:17 +0900

    Initial Commit

---
+++ MYcro HistoryTests/MYcro_HistoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MYcro_HistoryTests.swift
+//  MYcro HistoryTests
+//
+//  Created by Rachel J.Kim on 3/9/26.
+//
+
+import Testing
+@testable import MYcro_History
+
+struct MYcro_HistoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
